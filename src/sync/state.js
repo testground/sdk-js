e44a805de12986c1e68dfe7c91b27964382afabf
Henrique Dias <hacdias@gmail.com>
2020-12-18 11:27:39 +0000

    docs: add types to sync
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/sync/state.js
+++ src/sync/state.js
@@ -2,112 +2,133 @@
 
 const { REDIS_PAYLOAD_KEY } = require('./redis')
 
+/** @typedef {import('../runtime').RunParams} RunParams */
+/** @typedef {import('./types').State} State */
+
+/**
+ * @param {number} ms
+ * @returns {Promise<void>}
+ */
 function sleep (ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms)
   })
 }
 
+/**
+ * @param {string} state
+ * @param {RunParams} params
+ * @returns {string}
+ */
 function stateKey (state, params) {
   return `run:${params.testRun}:plan:${params.testPlan}:case:${params.testCase}:states:${state}`
 }
 
+/**
+ * @param {RunParams} params
+ * @returns {string}
+ */
 function eventsKey (params) {
   return `run:${params.testRun}:plan:${params.testPlan}:case:${params.testCase}:run_events`
 }
 
-function barrier ({ logger, extractor, redis }) {
-  return async (state, target) => {
-    // a barrier with target zero is satisfied immediately; log a warning as
-    // this is probably programmer error.
-    if (target === 0) {
-      logger.warn('requested a barrier with target zero; satisfying immediately', { state })
+/**
+ * @param {import('winston').Logger} logger
+ * @param {function():Promise<RunParams>} extractor
+ * @param {import('ioredis').Redis} redis
+ * @returns {State}
+ */
+function createState (logger, extractor, redis) {
+  return {
+    barrier: async (state, target) => {
+      // a barrier with target zero is satisfied immediately; log a warning as
+      // this is probably programmer error.
+      if (target === 0) {
+        logger.warn('requested a barrier with target zero; satisfying immediately', { state })
+        return {
+          state,
+          key: '',
+          target: target,
+          cancel: () => {},
+          wait: Promise.resolve()
+        }
+      }
+
+      const params = await extractor()
+      if (!params) {
+        throw new Error('no run parameters provided')
+      }
+
+      const key = stateKey(state, params)
+      let run = true
+
+      const wait = (async () => {
+        // NOTE(hacdias): o sdk-go, we have a barrierWorker that fetches all barriers at once.
+        // For simplicity, I decided to create a async function here to wait for the result,
+        // so we'll be executing a READ for each ongoing barrier. I'm not completely sure if this
+        // is a good idea or if there is any specific reason why the Go implementation decided to go
+        // for a global barrier worker.
+
+        while (run) { // eslint-disable-line
+          const curr = await redis.get(key)
+
+          if (curr) {
+            const num = Number.parseInt(curr)
+
+            if (num >= target) {
+              logger.debug('barrier was hit; informing waiters', { key, target, curr })
+              return
+            } else {
+              logger.debug('barrier still unsatisfied', { key, target, curr })
+            }
+          }
+
+          await sleep(1000) // 1s
+        }
+      })()
+
+      const cancel = () => {
+        run = false
+      }
+
       return {
         state,
-        wait: Promise.resolve()
+        key,
+        target,
+        wait,
+        cancel
       }
-    }
-
-    const params = await extractor()
-    if (!params) {
-      throw new Error('no run parameters provided')
-    }
-
-    const key = stateKey(state, params)
-    let run = true
-
-    const wait = (async () => {
-      // NOTE(hacdias): o sdk-go, we have a barrierWorker that fetches all barriers at once.
-      // For simplicity, I decided to create a async function here to wait for the result,
-      // so we'll be executing a READ for each ongoing barrier. I'm not completely sure if this
-      // is a good idea or if there is any specific reason why the Go implementation decided to go
-      // for a global barrier worker.
-
-      while (run) { // eslint-disable-line
-        const curr = await redis.get(key)
-
-        if (curr >= target) {
-          logger.debug('barrier was hit; informing waiters', { key, target, curr })
-          return
-        } else {
-          logger.debug('barrier still unsatisfied', { key, target, curr })
-        }
-
-        await sleep(1000) // 1s
+    },
+    signalEntry: async (state) => {
+      const params = await extractor()
+      if (!params) {
+        throw new Error('no run parameters provided')
       }
-    })()
 
-    const cancel = () => {
-      run = false
+      const key = stateKey(state, params)
+      logger.debug('signalling entry to state', { key })
+
+      const seq = await redis.incr(key)
+      logger.debug('new value of state', { key, value: seq })
+
+      return seq
+    },
+    signalEvent: async (event) => {
+      const params = await extractor()
+      if (!params) {
+        throw new Error('no run parameters provided')
+      }
+
+      const key = eventsKey(params)
+      logger.debug('signalling event', { key, value: event })
+
+      const json = JSON.stringify(event)
+      await redis.xadd(key, '*', REDIS_PAYLOAD_KEY, json)
+      logger.debug('successfully signalled event', { key })
     }
-
-    return {
-      state,
-      key,
-      target,
-      wait,
-      cancel
-    }
-  }
-}
-
-function signalEntry ({ logger, extractor, redis }) {
-  return async (state) => {
-    const params = await extractor()
-    if (!params) {
-      throw new Error('no run parameters provided')
-    }
-
-    const key = stateKey(state, params)
-    logger.debug('signalling entry to state', { key })
-
-    const seq = await redis.incr(key)
-    logger.debug('new value of state', { key, value: seq })
-
-    return seq
-  }
-}
-
-function signalEvent ({ logger, extractor, redis }) {
-  return async (event) => {
-    const params = await extractor()
-    if (!params) {
-      throw new Error('no run parameters provided')
-    }
-
-    const key = eventsKey(params)
-    logger.debug('signalling event', { key, value: event })
-
-    const json = JSON.stringify(event)
-    await redis.xadd(key, '*', REDIS_PAYLOAD_KEY, json)
-    logger.debug('successfully signalled event', { key })
   }
 }
 
 module.exports = {
-  createState: (options) => ({
-    barrier: barrier(options),
-    signalEntry: signalEntry(options),
-    signalEvent: signalEvent(options)
-  })
+  createState
 }
---
+++ src/sync/types.ts
@@ -0,0 +1,40 @@
+export interface Barrier {
+  state: string
+  key: string
+  target: number
+  wait: Promise<void>
+  cancel: () => void
+}
+
+export interface State {
+  barrier: (state: string, target: number) => Promise<Barrier>
+  signalEntry: (state: string) => Promise<number>
+  signalEvent: (event: any) => void
+}
+
+export interface Subscribe {
+  cancel: () => void
+  wait: AsyncGenerator<any, void, unknown>
+}
+
+export interface Topic {
+  publish: (topic: string, payload: any) => Promise<number>
+  subscribe: (topic: string) => Promise<Subscribe>
+}
+
+export type StateAndTopic = State & Topic
+
+export interface PublishSubscribe {
+  seq: number
+  sub: Subscribe
+}
+
+export interface Sugar {
+  publishAndWait: (topic: string, payload: any, state: string, target: number) => Promise<number>
+  publishSubscribe: (topic: string, payload: any) => Promise<PublishSubscribe>
+  signalAndWait: (state: string, target: number) => Promise<number>
+}
+
+export interface SyncClient extends State, Topic, Sugar {
+  close: () => void
+}
