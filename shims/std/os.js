ec61e2e4ea07d56a9365e504c1d4ee2d554c0ceb
glendc <glen.decauwsemaecker@otainsight.com>
2022-11-08 01:04:49 +0100

    continue shimming of codebase

---
+++ shims/std/os.js
@@ -0,0 +1,13 @@
+/**
+ * @returns {os.NetworkInterfaceInfo[]}
+ */
+export function networkInterfaces () {
+    return []
+}
+
+/**
+ * @returns {string}
+ */
+ export function hostname () {
+    return 'browser'
+}
