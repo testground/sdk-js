18576866761ccaaf926ef7058061929fa4fa5201
glendc <glen.decauwsemaecker@otainsight.com>
2022-11-10 00:47:01 +0100

    improve logger capabilities

--- src/shims/runtime/logger.js
+++ src/shims/runtime/logger.js
@@ -1,7 +1,20 @@
 'use strict'
 
 function getLogger () {
-  return console
+  return {
+    debug: function (...args) {
+      console.debug(...args)
+    },
+    info: function (...args) {
+      console.info(...args)
+    },
+    warn: function (...args) {
+      console.warn(...args)
+    },
+    error: function (...args) {
+      console.error(...args);
+    }
+  };
 }
 
 module.exports = {
