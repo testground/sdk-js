b1f6c49734c6455e146a8da889c8031915a2564c
glendc <glen.decauwsemaecker@otainsight.com>
2022-11-12 10:09:19 +0100

    use 'console' from WebAPI as is in browser for logging purposes

--- src/shims/runtime/logger.js
+++ src/shims/runtime/logger.js
@@ -1,20 +1,7 @@
 'use strict'
 
 function getLogger () {
-  return {
-    debug: function (...args) {
-      console.debug(...args)
-    },
-    info: function (...args) {
-      console.info(...args)
-    },
-    warn: function (...args) {
-      console.warn(...args)
-    },
-    error: function (...args) {
-      console.error(...args);
-    }
-  };
+  return console
 }
 
 module.exports = {
